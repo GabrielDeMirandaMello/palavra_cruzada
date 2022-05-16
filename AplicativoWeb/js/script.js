30a0e93a8522134c2fcb5be9b1b3c092e7970055
Lucas Penalva <lucas.pfranca@sptech.school>
2022-05-15 23:47:50 -0300

    validações das palavras concluídas

--- AplicativoWeb/js/script.js
+++ AplicativoWeb/js/script.js
@@ -13,11 +13,11 @@ function verificar_eprom(){
         document.getElementById("o_eprom").style="color:green"
         document.getElementById("m_eprom").style="color:green"
     } else {
-        document.getElementById("e_eprom").style="color:red"
-        document.getElementById("p_eprom").style="color:red"
-        document.getElementById("r_eprom").style="color:red"
-        document.getElementById("o_eprom").style="color:red"
-        document.getElementById("m_eprom").style="color:red"
+        document.getElementById("e_eprom").style="color:black"
+        document.getElementById("p_eprom").style="color:black"
+        document.getElementById("r_eprom").style="color:black"
+        document.getElementById("o_eprom").style="color:black"
+        document.getElementById("m_eprom").style="color:black"
     }
 
     console.log(`${e} ${p} ${r} ${o} ${m}`)
@@ -61,20 +61,20 @@ function verificar_memoria_massa(){
             var s2 = document.getElementById("s3_memoria_massa").style = "color: green"
             var a3 = document.getElementById("a3_memoria_massa").style = "color: green"
     } else {
-        var m1 = document.getElementById("m_eprom").style = "color: red"
-        var e1 = document.getElementById("e_memoria_massa").style = "color: red"
-        var m2 = document.getElementById("m_memoria_massa").style = "color: red"
-        var o = document.getElementById("o_memoria_massa").style = "color: red"
-        var r = document.getElementById("r_memoria_massa").style = "color: red"
-        var i = document.getElementById("i_memoria_massa").style = "color: red"
-        var a1 = document.getElementById("a_memoria_massa").style = "color: red"
-        var d = document.getElementById("d_memoria_massa").style = "color: red"
-        var e2 = document.getElementById("e_memoria_massa").style = "color: red"
-        var m3 = document.getElementById("m_memoria_massa").style = "color: red"
-        var a2 = document.getElementById("a_memoria_massa").style = "color: red"
-        var s1 = document.getElementById("s_memoria_massa").style = "color: red"
-        var s2 = document.getElementById("s_memoria_massa").style = "color: red"
-        var a3 = document.getElementById("a_memoria_massa").style = "color: red"
+        var m1 = document.getElementById("m_eprom").style = "color: black"
+        var e1 = document.getElementById("e_memoria_massa").style = "color: black"
+        var m2 = document.getElementById("m_memoria_massa").style = "color: black"
+        var o = document.getElementById("o_memoria_massa").style = "color: black"
+        var r = document.getElementById("r_memoria_massa").style = "color: black"
+        var i = document.getElementById("i_memoria_massa").style = "color: black"
+        var a1 = document.getElementById("a_memoria_massa").style = "color: black"
+        var d = document.getElementById("d_memoria_massa").style = "color: black"
+        var e2 = document.getElementById("e_memoria_massa").style = "color: black"
+        var m3 = document.getElementById("m_memoria_massa").style = "color: black"
+        var a2 = document.getElementById("a_memoria_massa").style = "color: black"
+        var s1 = document.getElementById("s_memoria_massa").style = "color: black"
+        var s2 = document.getElementById("s_memoria_massa").style = "color: black"
+        var a3 = document.getElementById("a_memoria_massa").style = "color: black"
     }
 }
 
@@ -93,11 +93,11 @@ function verificar_flash(){
         document.getElementById("s_flash").style="color:green"
         document.getElementById("h_flash").style="color:green"
     } else {
-        document.getElementById("f_flash").style="color:red"
-        document.getElementById("l_flash").style="color:red"
-        document.getElementById("a_flash").style="color:red"
-        document.getElementById("s_flash").style="color:red"
-        document.getElementById("h_flash").style="color:red"
+        document.getElementById("f_flash").style="color:black"
+        document.getElementById("l_flash").style="color:black"
+        document.getElementById("a_flash").style="color:black"
+        document.getElementById("s_flash").style="color:black"
+        document.getElementById("h_flash").style="color:black"
     }
 }
 
@@ -124,13 +124,13 @@ function verificar_adressbus(){
         document.getElementById("u_adressbus").style="color:green"
         document.getElementById("s3_adressbus").style="color:green"
     } else {
-        document.getElementById("a_flash").style="color:red"
-        document.getElementById("d_adressbus").style="color:red"
-        document.getElementById("e_adressbus").style="color:red"
-        document.getElementById("s_adressbus").style="color:red"
+        document.getElementById("a_flash").style="color:black"
+        document.getElementById("d_adressbus").style="color:black"
+        document.getElementById("e_adressbus").style="color:black"
+        document.getElementById("s_adressbus").style="color:black"
         document.getElementById("s2_adressbus").style="color:green"
-        document.getElementById("b_adressbus").style="color:red"
-        document.getElementById("u_adressbus").style="color:red"
+        document.getElementById("b_adressbus").style="color:black"
+        document.getElementById("u_adressbus").style="color:black"
     }
     
     console.log(`${a} ${d} ${r} ${e} ${s1} ${s2} ${b} ${u} ${s3}`)
@@ -147,9 +147,9 @@ function verificar_amd(){
         document.getElementById("m_amd").style="color:green"
         document.getElementById("a_amd").style="color:green"
     } else {
-        document.getElementById("d_amd").style="color:red"
-        document.getElementById("m_amd").style="color:red"
-        document.getElementById("a_amd").style="color:red"
+        document.getElementById("d_amd").style="color:black"
+        document.getElementById("m_amd").style="color:black"
+        document.getElementById("a_amd").style="color:black"
     }
 }
 
@@ -173,13 +173,13 @@ function verificar_databus(){
             document.getElementById("u_databus").style="color:green"
             document.getElementById("s2_adressbus").style="color:green"
     } else {
-        document.getElementById("d_memoria_massa").style="color:red"
-        document.getElementById("a_databus").style="color:red"
-        document.getElementById("t_databus").style="color:red"
-        document.getElementById("a_amd").style="color:red"
-        document.getElementById("b_databus").style="color:red"
-        document.getElementById("u_databus").style="color:red"
-        document.getElementById("s2_adressbus").style="color:red"
+        document.getElementById("d_memoria_massa").style="color:black"
+        document.getElementById("a_databus").style="color:black"
+        document.getElementById("t_databus").style="color:black"
+        document.getElementById("a_amd").style="color:black"
+        document.getElementById("b_databus").style="color:black"
+        document.getElementById("u_databus").style="color:black"
+        document.getElementById("s2_adressbus").style="color:black"
     }
     console.log(`${d} ${a1} ${t} ${a2} ${b} ${u} ${s}`)
 }
@@ -199,46 +199,149 @@ function verificar_isete(){
         document.getElementById("t_isete").style = "color:green"
         document.getElementById("e2_isete").style = "color:green"
     } else {
-        document.getElementById("i_isete").style = "color:red"
-        document.getElementById("s_isete").style = "color:red"
-        document.getElementById("e1_isete").style = "color:red"
-        document.getElementById("t_isete").style = "color:red"
-        document.getElementById("e2_isete").style = "color:red"
+        document.getElementById("i_isete").style = "color:black"
+        document.getElementById("s_isete").style = "color:black"
+        document.getElementById("e1_isete").style = "color:black"
+        document.getElementById("t_isete").style = "color:black"
+        document.getElementById("e2_isete").style = "color:black"
     }
 
 }
 
 // 8
 function verificar_cs(){
+    var c = document.getElementById("c_cs").value
 
+    if (c == "c") {
+        document.getElementById("c_cs").style = "color:green"
+    }
+    else {
+        document.getElementById("c_cs").style = "color:black"
+    }
 }
 
 // 9
 function verificar_icinco(){
+    var i1 = document.getElementById("i1_icinco").value
+    var c1 = document.getElementById("c1_icinco").value
+    var i2 = document.getElementById("i2_icinco").value
+    var n = document.getElementById("n_icinco").value
+    var c2 = document.getElementById("c2_icinco").value
+    var o = document.getElementById("o_icinco").value
 
+    if (i1 == "i" && c1 == "c" && i2 == "i" && n == "n" && c2 == "c" && o == "o") {
+        document.getElementById("i1_icinco").style = "color:green"
+        document.getElementById("c1_icinco").style = "color:green"
+        document.getElementById("i2_icinco").style = "color:green"
+        document.getElementById("n_icinco").style = "color:green"
+        document.getElementById("c2_icinco").style = "color:green"
+        document.getElementById("o_icinco").style = "color:green"
+    }
+    else {
+        document.getElementById("i1_icinco").style = "color:black"
+        document.getElementById("c1_icinco").style = "color:black"
+        document.getElementById("i2_icinco").style = "color:black"
+        document.getElementById("n_icinco").style = "color:black"
+        document.getElementById("c2_icinco").style = "color:black"
+        document.getElementById("o_icinco").style = "color:black"
+    }
 }
 
 // 10
 function verificar_ula(){
+    var u = document.getElementById("u_ula").value
+    var l = document.getElementById("l_ula").value
+    var a = document.getElementById("a_ula").value
 
+    if (u == "u" && l == "l" && a == "a") {
+        document.getElementById("u_ula").style = "color:green"
+        document.getElementById("l_ula").style = "color:green"
+        document.getElementById("a_ula").style = "color:green"
+    }
+    else {
+        document.getElementById("u_ula").style = "color:black"
+        document.getElementById("l_ula").style = "color:black"
+        document.getElementById("a_ula").style = "color:black"
+    }
 }
 
 // 11
 function verificar_registradores(){
+    var r1 = document.getElementById("r1_registradores").value
+    var g = document.getElementById("g_registradores").value
+    var s1 = document.getElementById("s1_registradores").value
+    var t = document.getElementById("t_registradores").value
+    var r2 = document.getElementById("r2_registradores").value
+    var d = document.getElementById("d_registradores").value
+    var o = document.getElementById("o_registradores").value
+    var r3 = document.getElementById("r3_registradores").value
+    var e = document.getElementById("e_registradores").value
 
+    if (r1 == "r" && g == "g" && s1 == "s" && t == "t" && r2 == "r" && d == "d" && o == "o" && r3 == "r" && e == "e") {
+        document.getElementById("r1_registradores").style = "color:green"
+        document.getElementById("g_registradores").style = "color:green"
+        document.getElementById("s1_registradores").style = "color:green"
+        document.getElementById("t_registradores").style = "color:green"
+        document.getElementById("r2_registradores").style = "color:green"
+        document.getElementById("d_registradores").style = "color:green"
+        document.getElementById("o_registradores").style = "color:green"
+        document.getElementById("r3_registradores").style = "color:green"
+        document.getElementById("e_registradores").style = "color:green"
+    }
+    else {
+        document.getElementById("r1_registradores").style = "color:black"
+        document.getElementById("g_registradores").style = "color:black"
+        document.getElementById("s1_registradores").style = "color:black"
+        document.getElementById("t_registradores").style = "color:black"
+        document.getElementById("r2_registradores").style = "color:black"
+        document.getElementById("d_registradores").style = "color:black"
+        document.getElementById("o_registradores").style = "color:black"
+        document.getElementById("r3_registradores").style = "color:black"
+        document.getElementById("e_registradores").style = "color:black"
+    }
 }
 
 // 12 
 function verificar_ram(){
+    var a = document.getElementById("a_ram").value
+    var m = document.getElementById("m_ram").value
 
+    if(a == "a" && m == "m") {
+        document.getElementById("a_ram").style = "color:green"
+        document.getElementById("m_ram").style = "color:green"
+    }
+    else {
+        document.getElementById("a_ram").style = "color:black"
+        document.getElementById("m_ram").style = "color:black"
+    }
 }
 
 // 13
 function verificar_cpu(){
+    p = document.getElementById("p_cpu").value
+    u = document.getElementById("u_cpu").value
 
+    if(p == "p" && u == "u") {
+        document.getElementById("p_cpu").style = "color:green"
+        document.getElementById("u_cpu").style = "color:green"
+    }
+    else {
+        document.getElementById("p_cpu").style = "color:black"
+        document.getElementById("u_cpu").style = "color:black"
+    }
 }
 
 // 14
 function verificar_rom(){
+    var r = document.getElementById("r_rom").value
+    var m = document.getElementById("m_rom").value
 
+    if (r == "r" && m == "m") {
+        document.getElementById("r_rom").style = "color:green"
+        document.getElementById("m_rom").style = "color:green"
+    }
+    else {
+        document.getElementById("r_rom").style = "color:black"
+        document.getElementById("m_rom").style = "color:black"
+    }
 }
